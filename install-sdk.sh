acb429a2656b6b81c21d0ad4a46a421187e9c12c
Marcello de Sales <Marcello_DeSales@intuit.com>
2015-04-02 14:54:01 -0700

    CI/CD: Updating the android update command to only use 21 version: 0.0.9 box
    Reviewing the list again, I removed the top versions in favor of API 21.
    
       5- Android SDK Build-tools, revision 21.1.2
      23- SDK Platform Android 5.0.1, API 21, revision 2
     114- Google APIs ARM EABI v7a System Image, Google Inc. API 21, revision 4
     116- Google APIs Intel x86 Atom System Image, Google Inc. API 21, revision 4

--- install-sdk.sh
+++ install-sdk.sh
@@ -27,4 +27,4 @@ echo "export PATH=\"${PATH}:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools
 do
   echo 'y'
   sleep 2
-done) | android update sdk -u -a -t 1,2,3,22,114,116
+done) | android update sdk -u -a -t 5,23,114,116
